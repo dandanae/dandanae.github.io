1b13f95b6e77ff97a5f350f4604448e186d764c2
dandanae <m1kun6ng@gmail.com>
2025-07-09 22:36:01 +0900

    게시 글 카테고리 분리

---
+++ src/componets/post/PostCategory.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Category } from '@/libs/posts'
+
+import { Card } from '../common'
+
+const PostCategory = ({ categories }: { categories: Category[] }) => {
+  return (
+    <Card variant="primary" classname="ml-auto lg:w-2/3">
+      <div className="flex flex-col gap-5">
+        <div className="text-primary dark:bg-primary/40 text-center text-lg font-semibold">
+          CATEGORY
+        </div>
+        {categories.map((category: Category) => (
+          <div key={category.name} className="group flex cursor-pointer items-center gap-3">
+            <span className="group-hover:text-secondary transition-all duration-300 group-hover:text-lg group-hover:font-medium">
+              {category.name}
+            </span>
+            <div className="border-b-primary group-hover:border-b-secondary h-0 w-full border-b border-dashed transition-colors duration-300" />
+            <span className="ring-primary/50 group-hover:ring-secondary/50 group-hover:bg-secondary/10 rounded-full px-4 py-1 text-xs ring transition-colors duration-300">
+              {category.count}
+            </span>
+          </div>
+        ))}
+      </div>
+    </Card>
+  )
+}
+
+export default PostCategory
--- src/componets/post/PostPage.tsx
+++ src/componets/post/PostPage.tsx
@@ -7,29 +7,13 @@ import type { Category } from '@/libs/posts'
 
 import PostCard from './PostCard'
 import { Card } from '../common'
+import PostCategory from './PostCategory'
 import PostList from './PostList'
 
 const PostPage = ({ posts, categories }: { posts: any; categories: any }) => {
   return (
     <>
-      <Card variant="primary" classname="ml-auto lg:w-2/3">
-        <div className="flex flex-col gap-5">
-          <div className="text-primary dark:bg-primary/40 text-center text-lg font-semibold">
-            CATEGORY
-          </div>
-          {categories.map((category: Category) => (
-            <div key={category.name} className="group flex cursor-pointer items-center gap-3">
-              <span className="group-hover:text-secondary transition-all duration-300 group-hover:text-lg group-hover:font-medium">
-                {category.name}
-              </span>
-              <div className="border-b-primary group-hover:border-b-secondary h-0 w-full border-b border-dashed transition-colors duration-300" />
-              <span className="ring-primary/50 group-hover:ring-secondary/50 group-hover:bg-secondary/10 rounded-full px-4 py-1 text-xs ring transition-colors duration-300">
-                {category.count}
-              </span>
-            </div>
-          ))}
-        </div>
-      </Card>
+      <PostCategory categories={categories} />
       <PostList title="최근 게시 글" posts={posts} />
     </>
   )
